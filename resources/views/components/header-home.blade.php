a1f2c0867ed000190ae6bd870c04579086dfd1cf
Wahyu NT <wahyuntx28@gmail.com>
2024-07-28 02:10:42 +0700

    Fix header component and update navbar links

--- resources/views/components/header-home.blade.php
+++ resources/views/components/header-home.blade.php
@@ -1,27 +1,24 @@
 <nav class="navbar navbar-expand-lg bg-light py-3 mb-1">
     <div class="container-fluid container">
-        <a class="navbar-brand text-primary fw-bold" href="#">SISMIOP</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <a class="navbar-brand " href="/">
+         <h3 class="text-primary fw-bold">SISMIOP</h3></a>
+
         <div class="d-flex justify-content-center">
 
             <div class="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active text-primary fw-bold" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active text-primary fw-bold" aria-current="page" href="#home">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Visi</a>
+                        <a class="nav-link" href="#visi">Visi</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Misi</a>
+                        <a class="nav-link" href="#misi">Misi</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Berita</a>
+                        <a class="nav-link" href="#berita">Berita</a>
                     </li>
-
                 </ul>
 
             </div>
